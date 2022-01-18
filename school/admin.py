1c058c6232854a17fa1fb046b63483447cffc19c
Guilherme Albuquerque <guialbquerque@gmail.com>
2022-01-18 06:05:18 -0300

    Created the admin model for each model

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,21 @@
+from re import search
 from django.contrib import admin
+from school.models import Student, Course
 
-# Register your models here.
+class Students(admin.ModelAdmin):
+
+    list_display = ('id', 'name', 'rg', 'cpf', 'date_birth')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 20
+
+admin.site.register(Student, Students)
+
+class Courses(admin.ModelAdmin):
+
+    list_display = ('id', 'code_course', 'description')
+    list_display_links = ('id', 'code_course')
+    search_fields = ('code_course',)
+
+admin.site.register(Course, Courses)
+
