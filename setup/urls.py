1f67d296758bfe85a84c3e0bb74e9d19b95d8d15
Guilherme Albuquerque <guialbquerque@gmail.com>
2022-01-18 19:51:11 -0300

    It were added the routers of the urls, now its possible visualize the models in brownser

--- setup/urls.py
+++ setup/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from school.views import students
+from django.urls import path, include
+from school.views import StudentsViewSet, CoursesViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('Students', StudentsViewSet, basename = 'Students')
+router.register('Courses', CoursesViewSet, basename = 'Courses' )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('students/', students),
+    path('', include(router.urls))
 ]
