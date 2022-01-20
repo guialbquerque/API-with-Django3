9a1b32031c77cff0815db27307f2d377d8dce57c
Guilherme Albuquerque <guialbquerque@gmail.com>
2022-01-20 16:18:51 -0300

    Created the view to model Registration

--- school/views.py
+++ school/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from school.models import Student, Course
-from school.serializer import StudentSerializer, CourseSerializer
+from school.models import Registration, Student, Course
+from school.serializer import StudentSerializer, CourseSerializer, RegistrationSerializer
 
 class StudentsViewSet(viewsets.ModelViewSet):
     """Showing all students"""
@@ -11,4 +11,9 @@ class CoursesViewSet(viewsets.ModelViewSet):
     """Showing all courses"""
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+class RegistrationsViewSet(viewsets.ModelViewSet):
+    """Showing all registrations"""
+    queryset = Registration.objects.all()
+    serializer_class = RegistrationSerializer
 
